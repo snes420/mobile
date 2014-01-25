b1aff706a9c1a66d475a7efcb298f02cf7a9efab
Kenneth Nierenhausen <vagrant@precise32.(none)>
2014-01-25 02:29:32 +0000

    splash screen centering

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,10 @@
 function pop() {
   alert("Thank you for showing interest :)");
 }
+
+function init() {
+  $('#splash').css('position', 'absolute');
+  $('#splash').css('top', '50%');
+  $('#splash').css('left', '10px');
+  $('#splash').css('right', '10px');
+}
