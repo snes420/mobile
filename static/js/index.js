c585f60ed77334cdd9cb1a0f1583b3780dee1aa0
Kenneth Nierenhausen <vagrant@precise32.(none)>
2014-02-05 18:19:15 +0000

    structure update and page refactor

--- static/js/index.js
+++ static/js/index.js
@@ -1,10 +1,23 @@
-function pop() {
-  alert("Thank you for showing interest :)");
+var ld_list = new Array();
+var count = 0;
+
+function init( ) {
+  ld_list.push("test biotch");
+  ld_list.push("test biotch again");
+  load();
 }
 
-function init() {
-  $('#splash').css('position', 'absolute');
-  $('#splash').css('top', '50%');
-  $('#splash').css('left', '10px');
-  $('#splash').css('right', '10px');
+function load() {
+  if(ld_list[count]) {
+    alert($("testing").append(ld_list[count]));
+    $("testing").text(ld_list[count]);
+    count = count + 1;
+    window.setTimeout("load()",200);
+  }
+  //window.setTimeout("redirect()",200);
 }
+
+function redirect() {
+  window.location="Home.html"
+  return;
+} 
